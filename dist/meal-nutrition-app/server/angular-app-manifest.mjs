96bf7c3dda9b781dc0fe3bf1cc1c68673aede204
Your Name <you@example.com>
2025-10-08 18:34:43 +0200

    pushing dist for demo

--- dist/meal-nutrition-app/server/angular-app-manifest.mjs
+++ dist/meal-nutrition-app/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 23562, hash: '1e0f68f108eee7dfc043f6e61e1fc3016c58a11e09f5241b9d83ad4e6d03da29', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17185, hash: 'fb40d2a003f658befbf6deacd9b1052d41a6bb15e4d0e9de74c0c10490766439', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23562, hash: '6239227e97b396d556a1cd0a268b028641e7f46983b5fe6411790b68ff138408', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17185, hash: '4300d4db60c21e83af28b037fb73b0ff05d967e756ebd171747167680a1dbc79', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-Z5UYKNDX.css': {size: 6934, hash: 'VxIyzD7LHG8', text: () => import('./assets-chunks/styles-Z5UYKNDX_css.mjs').then(m => m.default)}
   },
 };
